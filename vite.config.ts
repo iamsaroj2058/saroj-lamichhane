a3ee52340d21d104f58ad41d094ebc116ab6f1a8
saroj <saroj1229@xavier.edu.np>
2025-11-20 17:10:15 +0545

    Deployment chnages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/saroj-lamichhane/",   // 👈 REQUIRED FOR GitHub Pages
+  base: "/saroj-lamichhane/",  
   server: {
     host: "localhost",
     port: 5173,
